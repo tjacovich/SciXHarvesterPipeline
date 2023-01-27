c3af3b619536f75f6f8bd4e8de432fc6bb9eb095
tjacovich <tjacovich@cfa.harvard.edu>
2023-01-27 10:40:16 -0500

    Updated xml_parser structure.

--- harvester/xml_parsers.py
+++ harvester/xml_parsers.py
@@ -1,16 +1,35 @@
+from xml.dom import pulldom
+import logging as logger
+
 class arxiv_xml_parsers(object):
     @staticmethod
-    def parse_get_records():
+    def parse_list_records():
+        return True
+    
+    def parse_list_identifiers():
+        return True
+    
+    def parse_get_record():
         return True
 
-class Split_GetRecords(arxiv_xml_parsers):
+class Split_ListRecords(arxiv_xml_parsers):
     def __init__(self, raw_xml):
-        self.raw_xml = raw_xml
+        self.pull_parser = pulldom.parse(raw_xml)
+        self.resumptionToken = None
 
-    def __next__():
-        return True
+    def __next__(self):
+        for record in self.parse_list_records(self.pull_parser):
+            try:
+                split_record = ListRecord(record.get('identifier'), record.get('datestamp'), record.get('contents'))
+            except:
+                try:
+                    yield self.resumptionToken
+                except Exception as e:
+                    logger.error("Generator failed with exception: {}".format(e))
+                    
+            yield split_record 
 
-class GetRecord(object):
+class ListRecord(object):
     def __init__(self, identifier, datestamp, contents):
         self.identifier = identifier
         self.datestamp = datestamp
