bee4c7385b7628a8ee3018c725dfd7253243e3da
tjacovich <tjacovich@cfa.harvard.edu>
2023-01-30 11:10:44 -0500

    Updated requirements.txt and config.py for AWS.

--- config.py
+++ config.py
@@ -2,10 +2,19 @@
 #LOGGING_LEVEL = 'DEBUG'
 LOGGING_LEVEL = 'INFO'
 LOG_STDOUT = True
+#SQLALCHEMY Configuration
 SQLALCHEMY_URL = 'postgres://grpc_status:gRPC_status@postgres:5432/grpc_status'
 SQLALCHEMY_ECHO = False
+#REDIS Configuration
 REDIS_HOST = 'redis'
 REDIS_PORT = 6379
+#Kafka Configuration
 KAFKA_BROKER = 'kafka:9092'
 SCHEMA_REGISTRY_URL = 'http://schema-registry:8081'
+#Harvester AVRO schema ID
 SCHEMA_ID = '1'
+#AWS Configuration
+AWS_ACCESS_KEY_ID = 'CHANGEME'
+AWS_SECRET_ACCESS_KEY = 'SECRETS'
+REGION_NAME = 'us-east-1'
+PROFILE_NAME = 'SESSION_PROFILE'
