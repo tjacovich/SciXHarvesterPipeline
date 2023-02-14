aee76a373097dfbf07f066d777e3b8fce2c37fc8
tjacovich <tjacovich@cfa.harvard.edu>
2023-02-14 16:24:38 -0500

    Added docstrings and comments to arxiv_harvester.py. Modified some calls and rewrote certain methods.

--- harvester/metadata/arxiv_harvester.py
+++ harvester/metadata/arxiv_harvester.py
@@ -11,21 +11,31 @@ from datetime import datetime
 MAX_RETRIES = 5
 
 def arxiv_harvesting(app, job_request, config, producer):
+    """
+    Main harvesting routine for arxiv metadata. 
+    
+    job_request: (json) task message passed to Harvester input topic.
+    config: (json) The imported configuration of the Harvester app.
+    producer: The harvester kafka producer instance
+
+    return: (str) The final state of the harvesting process.
+    """
     datestamp = datetime.now()
     resumptionToken = job_request["task_args"].get("resumptionToken")
     harvester = ArXiV_Harvester(config.get("ARXIV_OAI_URL"), daterange=datestamp, resumptionToken=resumptionToken)
     harvester_output_schema = get_schema(app, app.schema_client, app.config.get('HARVESTER_OUTPUT_SCHEMA'))
     for record in harvester:
+        #Assign ID to new record
         record_id = uuid.uuid4()
+        #Generate filepath for S3
         file_path = "/{}/{}".format(datestamp, record_id)
-        
+        #write record to S3
         etag = app.s3_methods.write_object_s3(file_bytes=bytes(record), bucket=config.get('ARXIV_S3_BUCKET'), object_name=file_path)
         if etag:
             s3_key = file_path
             produce = db.write_harvester_record(app, record_id, record, datestamp, s3_key, etag, job_request.get("task"))
 
             if produce:
-                #placeholder code for producing to harvester output topic.
                 producer_message = {"record_id": record_id, "record_xml": record, "source": job_request.get("task")}
                 producer.produce(topic=config.get('HARVESTER_OUTPUT_TOPIC'), value=producer_message, value_schema=config.get('HARVESTER_OUTPUT_SCHEMA'))
         else:
@@ -35,15 +45,23 @@ def arxiv_harvesting(app, job_request, config, producer):
 
 class ArXiV_Harvester(OAI):
     def __init__(self, harvest_url, daterange, resumptionToken):
+        """
+        Initialization of harvesting class.
+
+        url: (str) The OAI harvesting url
+        params: (json) The required requests parameters
+        daterange: (str) A specified harvesting date range.
+        parsed_records: (array) Calls harvest method to produce an array of records accessible from the __next__ method.
+        """
         self.url = harvest_url
         self.params = {'metadataPrefix': 'oai_dc'}
-        self.daterange = None
-        self.raw_xml = ''
-        self.parsed_records = self.harvest_arxiv(harvest_url, daterange, resumptionToken)
+        self.daterange = daterange
+        self.raw_xml = None
+        self.parsed_records = self.harvest_arxiv(harvest_url, resumptionToken)
 
     @staticmethod
 
-    def harvest_arxiv(self, daterange, resumptionToken = None):
+    def harvest_arxiv(self, resumptionToken = None):
         """
         daterange: (str) date with value given as YYYY-MM-DD
         resumptionToken: (str) value returned by previous API call for paging.
@@ -57,11 +75,10 @@ class ArXiV_Harvester(OAI):
         while success != True:
             """
             This loop sends the relevant request to the ArXiV API and checks to make sure we aren't receiving any flow control responses.
-            If we are it waits the specified amount of time before proceeding. If we repeatedly hit 
+            If we are it waits the specified amount of time before proceeding. If we repeatedly hit 503 or any other error, we stop.
             """
             if not resumptionToken:
-                self.params['from'] = daterange
-                self.daterange = daterange
+                self.params['from'] = self.daterange
             
             else:
                 #specifying any other query params besides the verb with the resumptionToken will result in an error.
@@ -69,20 +86,30 @@ class ArXiV_Harvester(OAI):
 
             try:
                 raw_response = self.ListRecords(self.url, self.params)
-                raw_records = raw_response.content
+                self.raw_xml = raw_response.content
                 success = True
             except  Exception as e:
+                """
+                Still need to write the code that extracts the retry-after time from the response.
+                """
                 if raw_response.status_code == 503  and retries < MAX_RETRIES:
                     retries += 1 
                     sleep_time = 1
                     time.sleep(sleep_time)
                 else:
-                    logger.exception("Failed to Harvest ArXiV records for daterange: {}".format(daterange))
+                    logger.exception("Failed to Harvest ArXiV records for daterange: {}".format(self.daterange))
                     raise e
 
-            self.parsed_records = arxiv.ArxivParser.parse(raw_records)
+            self.parsed_records = arxiv.ArxivParser.parse(self.raw_xml)
 
     def __next__(self):
+        """
+        Iterate through all parsed records. 
+        If next(self.parsed_records) fails, we attempt to extract a resumptionToken and then rerun the harvest process with the token.
+        
+        return: 
+            record: (str) XML of a single ArXiv record
+        """
         try:
             record = next(self.parsed_records)
         except:
@@ -98,9 +125,16 @@ class ArXiV_Harvester(OAI):
 
     @staticmethod
     def extract_resumptionToken(raw_xml):
+        """
+        extracts the resumptionToken from the raw response. (ArXiv uses these for paging, see OAI harvesting docs for details).
+
+        raw_xml: (str) XML content from ArXiv
+
+        return: (str) ArXiv resumptionToken
+        """
         arxiv_parser = arxiv.MultiArxivParser()
         token_text = arxiv_parser.get_chunks(raw_xml, r"<resumptionToken", r"</resumptionToken>")
         pattern = re.compile(r"[0-9]+\|[0-9]+")
-        pattern.search(token_text)[0]
+        return pattern.search(token_text)[0]
 
 
