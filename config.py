41d7636f60898b367d4b0420e2ad100ac7f75d44
tjacovich <tjacovich@cfa.harvard.edu>
2023-02-15 14:03:45 -0500

    Updates to config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ REDIS_PORT = 6379
 #Kafka Configuration
 KAFKA_BROKER = 'kafka:9092'
 SCHEMA_REGISTRY_URL = 'http://schema-registry:8081'
-#Harvester AVRO schema ID
+#Harvester AVRO Schema Parameters
 SCHEMA_ID = '1'
 HARVESTER_INPUT_SCHEMA= 'Harvester'
 HARVESTER_INPUT_TOPIC = 'Harvester'
@@ -22,5 +22,8 @@ AWS_ACCESS_KEY_ID = 'CHANGEME'
 AWS_SECRET_ACCESS_KEY = 'SECRETS'
 AWS_DEFAULT_REGION = 'us-east-1'
 PROFILE_NAME = 'SESSION_PROFILE'
+#AWS Bucket Parameters
+BUCKET_NAME = 'BUCKETNAME'
+BUCKET_ARN = 'BUCKETARN'
 #OAI Harvesting urls
 ARXIV_OAI_URL='https://export.arxiv.org/oai2'
