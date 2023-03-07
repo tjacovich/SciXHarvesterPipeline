fe41faeb4ab3cea3cb9307cb48d017039a750ab1
tjacovich <tjacovich@cfa.harvard.edu>
2023-03-07 17:04:12 -0500

    Added check on raw xml for the nongenerator arxiv harvester tests.

--- tests/harvester/test_metadata_arxiv_harvester.py
+++ tests/harvester/test_metadata_arxiv_harvester.py
@@ -6,9 +6,15 @@ class test_ArXiV_Harvester(TestCase):
     def test_arxiv_harvester_from(self):
         with MockListRecords():
             arxiv_harvester = ArXiV_Harvester(harvest_url='https://export.arxiv.org/oai2', daterange='2023-03-07', resumptionToken=None)
+            with open('tests/data/ListRecords_data_0.xml', 'r+') as f:
+                response_text = f.read()    
+            self.assertEqual(arxiv_harvester.raw_xml, response_text)
     def test_arxiv_harvester_resumptionToken(self):
         with MockListRecords():
             arxiv_harvester = ArXiV_Harvester(harvest_url='https://export.arxiv.org/oai2', daterange=None, resumptionToken='6511260|1001')
+            with open('tests/data/ListRecords_data_1.xml', 'r+') as f:
+                response_text = f.read()    
+            self.assertEqual(arxiv_harvester.raw_xml, response_text)
     def test_arxiv_harvester_generator(self):
         with MockListRecords():
             arxiv_harvester = ArXiV_Harvester(harvest_url='https://export.arxiv.org/oai2', daterange='2023-03-07', resumptionToken=None)
