86b05691c311304f1191dbfaa38c976f8c455ab1
tjacovich <tjacovich@cfa.harvard.edu>
2023-03-08 10:18:37 -0500

    Added test for full harvest functionality.

--- tests/harvester/test_metadata_arxiv_harvester.py
+++ tests/harvester/test_metadata_arxiv_harvester.py
@@ -1,6 +1,7 @@
 from harvester.metadata.arxiv_harvester import ArXiV_Harvester
 from tests.harvester.base import MockListRecords
 from unittest import TestCase
+import pickle
 
 class test_ArXiV_Harvester(TestCase):
     def test_arxiv_harvester_from(self):
@@ -9,14 +10,22 @@ class test_ArXiV_Harvester(TestCase):
             with open('tests/data/ListRecords_data_0.xml', 'r+') as f:
                 response_text = f.read()    
             self.assertEqual(arxiv_harvester.raw_xml, response_text)
+    
     def test_arxiv_harvester_resumptionToken(self):
         with MockListRecords():
             arxiv_harvester = ArXiV_Harvester(harvest_url='https://export.arxiv.org/oai2', daterange=None, resumptionToken='6511260|1001')
             with open('tests/data/ListRecords_data_1.xml', 'r+') as f:
                 response_text = f.read()    
             self.assertEqual(arxiv_harvester.raw_xml, response_text)
+    
     def test_arxiv_harvester_generator(self):
         with MockListRecords():
             arxiv_harvester = ArXiV_Harvester(harvest_url='https://export.arxiv.org/oai2', daterange='2023-03-07', resumptionToken=None)
+            harvested_records = []
             for record in arxiv_harvester:
-                pass
+                harvested_records.append(record)
+            with open('tests/data/parsed_records.pkl', 'rb') as f:
+                test_records = pickle.load(f)
+            self.assertEqual(2265, len(harvested_records))
+            self.assertEqual(harvested_records, test_records)
+
