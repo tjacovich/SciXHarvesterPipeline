ce819eb54b15c8c224491765ba5a9c2526a8cbc1
tjacovich <tjacovich@cfa.harvard.edu>
2023-03-29 15:15:04 -0400

    Added additional chowns to entrypoint.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,6 +3,8 @@ mkdir -p /app/logs
 chown ads:ads /app/logs
 mkdir -p /app/logs/API
 mkdir -p /app/logs/APP
+chown ads:ads /app/logs/API
+chown ads:ads /app/logs/APP
 runsvdir -P /etc/service &
 tail -f /dev/null
 
