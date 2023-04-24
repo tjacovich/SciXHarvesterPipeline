e7349200cad2719728f410f67a494b0256a7b10f
tjacovich <tjacovich@cfa.harvard.edu>
2023-04-24 09:42:41 -0400

    Removed scripts and docker files from main repo.

--- postgres/init-db-once.sql
+++
@@ -1,2 +0,0 @@
-CREATE USER harvester WITH LOGIN PASSWORD 'harvester';
-GRANT CREATE ON DATABASE harvester TO harvester;
--- scripts/postgres/init-db-once.sql
+++
@@ -1,2 +0,0 @@
-CREATE ROLE harvester WITH LOGIN PASSWORD 'harvester';
-GRANT CREATE ON DATABASE harvester TO harvester;
